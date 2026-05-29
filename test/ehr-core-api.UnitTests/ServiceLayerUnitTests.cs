bac0af25ec2fb30b99174f2521265d4511d5bb34
Kyle Murray <kyle.w.murray@outlook.com>
2026-05-29 22:50:56 +0100

    complete three unit tests for add appointment method

--- test/ehr-core-api.UnitTests/ServiceLayerUnitTests.cs
+++ test/ehr-core-api.UnitTests/ServiceLayerUnitTests.cs
@@ -28,6 +28,110 @@ public class ServiceLayerUnitTests
         );
     }
 
+    // Add Appointment Unit Tests
+    [Fact]
+    public async Task AddAppointment_NullPatient_ReturnsFailureWithErrorMessage()
+    {
+        // Arrange
+        _mockPatientRepo.Setup(mock => mock.GetPatientAsync(1)).ReturnsAsync((Patient?)null);
+        
+        var mockCreateAppointmentDTO = new CreateAppointmentDTO
+        {
+            PatientId = 1,
+            Department = "TestDepartment",
+            ClinicianId = 5000,
+            AppointmentTime = new DateTime(2025, 2, 15, 12, 0, 0)
+        };
+
+        // Act
+        var result = await _appointmentService.AddAppointmentAsync(mockCreateAppointmentDTO);
+
+        // Assert
+        Assert.False(result.WasSuccessful);
+        Assert.Equal("Patient with this ID does not exist.", result.Message);
+    }
+
+    [Fact]
+    public async Task AddAppointment_NullClinician_ReturnsFailureWithErrorMessage()
+    {
+        // Arrange
+        Patient mockPatient = new Patient
+        {
+            Id = 1,
+            FirstName = "Test",
+            LastName = "Testson",
+            Dob = new DateOnly(1990, 10, 21),
+            NhsNumber = "0123456789",
+            Address = "101 Test Drive"
+        };
+
+        _mockPatientRepo.Setup(mock => mock.GetPatientAsync(1)).ReturnsAsync(mockPatient);
+        _mockClinicianRepo.Setup(mock => mock.GetClinicianAsync(1)).ReturnsAsync((Clinician?)null);
+        var mockCreateAppointmentDTO = new CreateAppointmentDTO
+        {
+            PatientId = 1,
+            Department = "TestDepartment",
+            ClinicianId = 1,
+            AppointmentTime = new DateTime(2024, 1, 12, 15, 0, 0)
+        };
+
+        // Act
+        var result = await _appointmentService.AddAppointmentAsync(mockCreateAppointmentDTO);
+
+        // Assert
+        Assert.False(result.WasSuccessful);
+        Assert.Equal("Clinician with this ID does not exist.", result.Message);
+    }
+
+    [Fact]
+    public async Task AddAppointment_ValidPatientClinicianAndAppointmentDTO_ReturnsSuccessWithNewAppointment()
+    {
+        // Arrange
+        Patient mockPatient = new Patient
+        {
+            Id = 3,
+            FirstName = "Test",
+            LastName = "Testson",
+            Dob = new DateOnly(1990, 10, 21),
+            NhsNumber = "0123456789",
+            Address = "101 Test Drive"
+        };
+
+        Clinician mockClinician = new Clinician
+        {
+            Id = 4,
+            FirstName = "Test",
+            LastName = "Testson",
+            Dob = new DateOnly(1990, 10, 21),
+            GmcNumber = "01234567",
+            Specialty = "Testing"
+        };
+
+        _mockPatientRepo.Setup(mock => mock.GetPatientAsync(3)).ReturnsAsync(mockPatient);
+        _mockClinicianRepo.Setup(mock => mock.GetClinicianAsync(4)).ReturnsAsync(mockClinician);
+        
+        var mockCreateAppointmentDTO = new CreateAppointmentDTO
+        {
+            PatientId = 3,
+            Department = "TestDepartment",
+            ClinicianId = 4,
+            AppointmentTime = new DateTime(2024, 1, 12, 15, 0, 0)
+        };
+
+        // Act
+        var result = await _appointmentService.AddAppointmentAsync(mockCreateAppointmentDTO);
+
+        // Assert
+        _mockAppointmentRepo.Verify(mock => mock.AddAndSaveAppointmentAsync(It.Is<Appointment>(a => a.ClinicianId == 4 
+                                                                                               && a.PatientId == 3
+                                                                                               && a.Department == "TestDepartment"
+                                                                                               && a.AppointmentTime ==  new DateTime(2024, 1, 12, 15, 0, 0)
+                                                                                               && a.Status == AppointmentStatus.Pending)), Times.Once);
+        Assert.True(result.WasSuccessful);
+        Assert.NotNull(result.NewAppointment);
+    }
+
+    // Change Appointment Status Unit Tests
     [Fact]
     public async Task ChangeAppointmentStatus_NullAppointment_ReturnsFailureWithMessage()
     {
@@ -47,7 +151,7 @@ public class ServiceLayerUnitTests
         var result = await _appointmentService.ChangeAppointmentStatus(mockChangeAppointmentDTO);
 
         // Assert
-         Assert.True(!result.wasSuccessful);
+         Assert.False(result.wasSuccessful);
          Assert.Equal("Appointment not found", result.message);
     }
 
