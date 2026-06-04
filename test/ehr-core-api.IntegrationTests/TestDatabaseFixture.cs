b3da17c1642935216664f0dd87fff032e2398d3f
Kyle Murray <kyle.w.murray@outlook.com>
2026-06-05 00:50:07 +0100

    create test database fixture for integration tests

---
+++ test/ehr-core-api.IntegrationTests/TestDatabaseFixture.cs
@@ -0,0 +1,6 @@
+﻿namespace ehr_core_api.IntegrationTests;
+
+public class TestDatabaseFixture
+{
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=EHRDb_IntegrationTests;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0";
+}
