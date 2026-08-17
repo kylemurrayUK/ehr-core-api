63427c988f008c3c636ddc508982e562e10dba37
Kyle Murray <kyle.w.murray@outlook.com>
2026-08-17 18:45:57 +0100

    add create appointment patient not found controller integration test

--- test/ehr-core-api.IntegrationTests/ControllerIntegrationTests/WriteControllerIntegrationTests.cs
+++ test/ehr-core-api.IntegrationTests/ControllerIntegrationTests/WriteControllerIntegrationTests.cs
@@ -1,5 +1,6 @@
 using EHRCoreAPI.Dtos;
 using EHRCoreAPI.Dtos.Output;
+using EHRCoreAPI.Models;
 using System.Net;
 
 
@@ -35,7 +36,26 @@ public class WriteControllerIntegrationTests : IDisposable
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         Assert.Contains("Patient Id is required.", responseBody);
         Assert.Contains("Clinician Id name is required.", responseBody);
+    }
+    [Fact]
+    public async Task CreateAppointment_PatientIdNotFound_Return400BadRequest()
+    {
+        //Arrange
+        var testIncorrectPatientIdAppointment = new CreateAppointmentDTO()
+        {
+            PatientId = 99999,
+            ClinicianId = 1,
+            Department = "TestDepartment",
+            AppointmentTime = new DateTime(2026, 8, 18, 10, 0, 0)
+        };
+        
+        //Act
+        var response = await _client.PostAsJsonAsync("api/Appointment/CreateAppointment", testIncorrectPatientIdAppointment);
+        var responseBody = await response.Content.ReadAsStringAsync();
 
+        //Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.Contains("Patient with this ID does not exist.", responseBody);
     }
 
     public void Dispose()
